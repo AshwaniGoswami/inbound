2dfe4f14e44d7be75b9e79638b683466358664c4
Arpit Verma <107420889+ArpitVerma12@users.noreply.github.com>
2024-02-13 17:54:39 +0530

    helo

--- src/main/java/com/kpdcl/inbound/InboundApplication.java
+++ src/main/java/com/kpdcl/inbound/InboundApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class InboundApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(InboundApplication.class, args);
 	}
 
