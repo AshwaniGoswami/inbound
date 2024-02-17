362428b218da9a90627956b9ac23fd4b9d59a7aa
Arpit Verma <107420889+ArpitVerma12@users.noreply.github.com>
2024-02-17 10:45:59 +0530

    change

--- src/main/java/com/kpdcl/inbound/controller/service.java
+++ src/main/java/com/kpdcl/inbound/controller/service.java
@@ -1,37 +1,37 @@
-package com.kpdcl.inbound.controller;
-
-import java.util.Map;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.kpdcl.inbound.token.createToken;
-
-@RestController
-public class service {
-
-    @Autowired
-    private createToken tokenGenerator;
-
-    @PostMapping("/api/data")
-    public ResponseEntity<String> createData(@RequestBody Map<String, Object> jsonData) {
-        try {
-        	//System.out.println(jsonData);
-            // Generate a JWT token using the JSON data
-            String token = tokenGenerator.generateToken(jsonData);
-            //System.out.println(token);
-            // Return the token along with a success status
-            return new ResponseEntity<>(token, HttpStatus.CREATED);
-        } catch (Exception e) {
-            // If there is an error, return an appropriate HTTP status code
-            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
-}
+//package com.kpdcl.inbound.controller;
+//
+//import java.util.Map;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import com.kpdcl.inbound.token.createToken;
+//
+//@RestController
+//public class service {
+//
+//    //@Autowired
+//    //private createToken tokenGenerator;
+//
+//    @PostMapping("/api/data")
+//    public ResponseEntity<String> createData(@RequestBody Map<String, Object> jsonData) {
+//        try {
+//        	//System.out.println(jsonData);
+//            // Generate a JWT token using the JSON data
+//            //String token = tokenGenerator.generateToken(jsonData);
+//            //System.out.println(token);
+//            // Return the token along with a success status
+//            return new ResponseEntity<>(token, HttpStatus.CREATED);
+//        } catch (Exception e) {
+//            // If there is an error, return an appropriate HTTP status code
+//            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+//        }
+//    }
+//}
 
 
 //package com.kpdcl.inbound.controller;
@@ -57,6 +57,74 @@ public class service {
 //	        }
 //	    }
 //}
+
+
+package com.kpdcl.inbound.controller;
+
+
+import com.kpdcl.inbound.token.createToken;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class service {
+
+    private final createToken createtoken;
+
+    @Autowired
+    public service(createToken createtoken) {
+        this.createtoken = createtoken;
+    }
+
+    @PostMapping("/api/data")
+    public ResponseEntity<ResponseObject> createData(@RequestBody Map<String, Object> jsonData) {
+        try {
+            // Generate token using TokenService
+            String token = createtoken.generateToken(jsonData);
+            System.out.println(token);
+            // Create a response object including the token and any other necessary data
+            ResponseObject responseObject = new ResponseObject();
+            responseObject.setToken(token);
+            responseObject.setData(jsonData); // Assuming you want to return the JSON data as well
+
+            // Return the response with HTTP status 201 (CREATED)
+            return new ResponseEntity<>(responseObject, HttpStatus.CREATED);
+        } catch (Exception e) {
+            // If there is an error, return an appropriate HTTP status code
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    // Define a ResponseObject class to include token and data
+    private static class ResponseObject {
+        private String token;
+        private Map<String, Object> data;
+
+        // Getter and setter methods
+        public String getToken() {
+            return token;
+        }
+
+        public void setToken(String token) {
+            this.token = token;
+        }
+
+        public Map<String, Object> getData() {
+            return data;
+        }
+
+        public void setData(Map<String, Object> data) {
+            this.data = data;
+        }
+    }
+}
 
 
 
--- src/main/java/com/kpdcl/inbound/token/createToken.java
+++ src/main/java/com/kpdcl/inbound/token/createToken.java
@@ -1,69 +1,95 @@
-
 //package com.kpdcl.inbound.token;
 //
+//import io.jsonwebtoken.Jwts;
+//import io.jsonwebtoken.SignatureAlgorithm;
+//import io.jsonwebtoken.io.Decoders;
+//import io.jsonwebtoken.security.Keys;
 //import org.springframework.beans.factory.annotation.Value;
 //import org.springframework.stereotype.Component;
 //
-//
-//
+//import javax.crypto.SecretKey;
+//import java.security.Key;
 //import java.util.Date;
 //import java.util.Map;
 //
 //@Component
 //public class createToken {
-//    @Value("${jwt.secret}")
-//    private String SECRET_KEY;
 //
-//    private static final long EXPIRATION_TIME_MS = 3600000; // 1 hour
+//    @Value("${jwt.expiration}")
+//    private Long jwtExpiration;
+//
+//    @Value("${jwt.secret}")
+//    private String secretKey;
 //
 //    public String generateToken(Map<String, Object> jsonData) {
-//        
-//		// Create JWT token using the JSON data
-//        String token = Jwts.builder()
-//                .setClaims(jsonData)
-//                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
-//                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME_MS))
+//        Date currentDate = new Date();
+//        Date expirationDate = new Date(currentDate.getTime() + jwtExpiration);
+//
+//        return Jwts.builder()
+//                .setPayload(jsonData) // Set the JSON data as the payload directly
+//                .setIssuedAt(currentDate)
+//                .setExpiration(expirationDate)
+//                .signWith(key(), SignatureAlgorithm.HS256)
 //                .compact();
-//        return token;
+//    }
+//
+//    private Key key() {
+//        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(secretKey));
 //    }
 //}
 
+
 package com.kpdcl.inbound.token;
 
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.io.Decoders;
-import io.jsonwebtoken.security.Keys;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
 
-import javax.crypto.SecretKey;
-import java.security.Key;
-import java.util.Date;
+import org.springframework.stereotype.Service;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Map;
 
-@Component
+@Service
 public class createToken {
 
-    @Value("${jwt.expiration}")
-    private Long jwtExpiration;
-
-    @Value("${jwt.secret}")
-    private String secretKey;
-
-    public String generateToken(String jsonData) {
-        Date currentDate = new Date();
-        Date expirationDate = new Date(currentDate.getTime() + jwtExpiration);
-
-        return Jwts.builder()
-                .setPayload(jsonData) // Set the JSON data as the payload directly
-                .setIssuedAt(currentDate)
-                .setExpiration(expirationDate)
-                .signWith(key(), SignatureAlgorithm.HS256)
-                .compact();
+    // Method to generate token based on JSON data
+    public String generateToken(Map<String, Object> jsonData) {
+        // Your token generation logic here
+        // Example: You can serialize the JSON data to a string and hash it to generate a token
+        String serializedData = serializeData(jsonData);
+        String token = hash(serializedData);
+        return token;
     }
 
-    private Key key() {
-        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(secretKey));
+    // Method to serialize JSON data to a string
+    private String serializeData(Map<String, Object> jsonData) {
+        // Your serialization logic here
+        // Example: Convert JSON data to a JSON string
+        StringBuilder stringBuilder = new StringBuilder();
+        for (Map.Entry<String, Object> entry : jsonData.entrySet()) {
+            stringBuilder.append(entry.getKey()).append(":").append(entry.getValue()).append(",");
+        }
+        return stringBuilder.toString();
+    }
+
+    // Method to hash the serialized data to generate a token
+    private String hash(String serializedData) {
+        // Your hash function implementation here
+        // Example: Use MessageDigest to hash the serialized data
+        // This is just a placeholder, replace it with your actual implementation
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] hash = digest.digest(serializedData.getBytes());
+            StringBuilder hexString = new StringBuilder();
+            for (byte b : hash) {
+                String hex = Integer.toHexString(0xff & b);
+                if (hex.length() == 1) hexString.append('0');
+                hexString.append(hex);
+            }
+            return hexString.toString();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
+
 
